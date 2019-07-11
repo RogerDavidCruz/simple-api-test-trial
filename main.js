42a051ab85b6ff390fe3203fec38d4ab1a7b6d4c
Roger David Cruz <roger.david.cruz@gmail.com>
2019-07-11 11:17:38 -0400

    updates

--- main.js
+++ main.js
@@ -31,23 +31,37 @@
 //new try after Nick help + Leon help
 //for annonymous function, remember that it is a function without a name, it can be placed upward and become an additional parameter with submit
 
+
+// NOTES
+// search=field:term
+
 document.querySelector('form').addEventListener('submit',function (e){ //e stands for event, an element in each e, e prevents default
   e.preventDefault()
   //e.preventDefaults prevents all the events from reload
   let drugName = document.querySelector('input').value
   console.log(drugName)
-  fetch(`https://api.fda.gov/drug/ndc.json?count=brand_name`)
+  fetch(`https://api.fda.gov/drug/ndc.json?search=generic_name:"${drugName}"&limit=1`)
+  // console.log(url)
+  // fetch(url)
   .then(res => res.json())
   .then(response => {
     console.log(response);
-    document.querySelector('#result1').textContent = response.results[0].term;
-    document.querySelector('#result2').textContent = response.meta.disclaimer;
+    document.querySelector('#result1').textContent = response.results[0].brand_name; //[0].generic_name
+    document.querySelector('#result2').textContent = response.results[0].pharm_class;
+    document.querySelector('#result3').textContent = response.results[0].dosage_form;
+    document.querySelector('#result4').textContent = response.results[0].route[0];
+    document.querySelector('#result5').textContent = response.meta.disclaimer;
   })
+//^Not sure if it's the best practice, maybe a for loop can be used in order to repeatedly get different responses or be written in one line?
+//Final Code notes
+
   // .catch(err =>{
   //   console.log(`error ${err}`)
   //   alert("Sorry, there are no results for your search.")
   // })
 })
+
+
 
 // function getMedReaction(e){ //e stands for event, an element in each e, e prevents default
 //   e.preventDefault()
